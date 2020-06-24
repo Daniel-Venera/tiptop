1cd01ada1797a31fcaf9a0bd79d7a018bba6f6ca
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-06-24 11:23:58 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      amount: null,
+      tip: null
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state.amount)
+  }
+
+  render() {
+    let tipAmount = (this.state.amount * (this.state.tip / 100))
+    let total = (parseInt(this.state.amount) + tipAmount).toFixed(2)
+    return (
+      <div id='main-container'>
+        <Navbar />
+        <div id='container'>
+          <div>
+          <label>Amount</label>
+          <br></br>
+          <input name='amount' type='number' value={this.state.amount} onChange={this.handleChange}></input>
+          </div>
+          <div>
+          <label>Tip</label>
+          <br></br>
+          <input name='tip' type='number' placeholder='ex: 10%' value={this.state.tip} onChange={this.handleChange}></input>
+          </div>
+          <div>Tip : <br></br><br></br><span>{this.state.amount ? tipAmount.toFixed(2) : null} </span></div>
+          <div>Total amount : <br></br><br></br><span id='total'>{this.state.amount ? total : null}</span> </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      Created with <i class="fa fa-heart"></i> by <a href="https://www.linkedin.com/in/daniel-venera/">Daniel Venera</a>
+      <br></br>
+      <a href='https://github.com/Daniel-Venera/tiptop' target='blank'><i class="fa fa-github"></i></a>
+      <a href="mailto:daniel.venera2@gmail.com" target="blank"><i class="fa fa-envelope-o"></i></a>
+      <a href="https://www.linkedin.com/in/daniel-venera/" target="blank"><i class="fa fa-linkedin"></i></a>
+    </footer>
+  )
+}
+
+export default Footer

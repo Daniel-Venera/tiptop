d03850ae9ce05f845ec44105ea887a3a7ca4bcc7
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-06-24 11:29:17 +0200

    Updates

---
+++ precache-manifest.75d362d9d2fc228fa18dbfa92ec66fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130e8f71b20c39838e3695f865e9c881",
+    "url": "/tiptop/index.html"
+  },
+  {
+    "revision": "b83d72bb2ca79f4f55ba",
+    "url": "/tiptop/static/css/main.d543e2f6.chunk.css"
+  },
+  {
+    "revision": "55798b1a92fe689b1366",
+    "url": "/tiptop/static/js/2.8498bf51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tiptop/static/js/2.8498bf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83d72bb2ca79f4f55ba",
+    "url": "/tiptop/static/js/main.6459bdb2.chunk.js"
+  },
+  {
+    "revision": "0ffe0e8cff8efd81d718",
+    "url": "/tiptop/static/js/runtime-main.1600cb82.js"
+  }
+]);
